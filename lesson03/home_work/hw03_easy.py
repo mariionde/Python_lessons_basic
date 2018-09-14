b5af42b30f137c334cf2f14011d0bbc92e9b2279
Mariionde <school@fonmatte.com>
2018-09-14 09:12:42 +0300

    Сделала часть заданий, времени на остальные  не хватило, хотела бы доделать их позднее.

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -1,16 +1,16 @@
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
-# Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
+# Округление должно происходить по математическим правилам (0.5 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
     pass
 
 
-print(my_round(2.1234567, 5))
-print(my_round(2.1999967, 5))
-print(my_round(2.9999967, 5))
+print(my_round(2.1234567, 5)) # 2.12346
+print(my_round(2.1999967, 5)) # 2.20000
+print(my_round(2.9999967, 5)) # 3.00000
 
 
 # Задание-2:
@@ -19,8 +19,23 @@ print(my_round(2.9999967, 5))
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
+
 def lucky_ticket(ticket_number):
-    pass
+
+    ticket_number = str(ticket_number)
+    ticket_list = []
+
+    for digit in ticket_number:
+        ticket_list.append(int(digit))
+
+    a = ticket_list[0:3]
+    b = ticket_list[3:6]
+    left_sum = sum(a)
+    right_sum = sum(b)
+    if left_sum == right_sum:
+        return ("Ваш билет выиграл")
+    else:
+        return ("Вам обязательно повезет в следующий раз!")
 
 
 print(lucky_ticket(123006))
